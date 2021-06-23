3be55e05d0168ee896b7877b416593e64a979233
hvalmer <hvalmer@gmail.com>
2021-06-23 15:38:42 -0300

    DTO

--- backend/src/main/java/com/baincustom/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/baincustom/dscatalog/resources/CategoryResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.baincustom.dscatalog.entities.Category;
+import com.baincustom.dscatalog.dto.CategoryDTO;
 import com.baincustom.dscatalog.services.CategoryService;
 
 /**
@@ -26,8 +26,8 @@ public class CategoryResource {
 	
 	//criando os endpoints
 	@GetMapping
-	public ResponseEntity<List <Category>> findAll(){
-		List<Category> list = service.findAll();
+	public ResponseEntity<List <CategoryDTO>> findAll(){
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
--- backend/src/main/java/com/baincustom/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/baincustom/dscatalog/services/CategoryService.java
@@ -1,13 +1,13 @@
 package com.baincustom.dscatalog.services;
 
 import java.util.List;
-
-
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.baincustom.dscatalog.dto.CategoryDTO;
 import com.baincustom.dscatalog.entities.Category;
 import com.baincustom.dscatalog.repositories.CategoryRepository;
 
@@ -19,7 +19,16 @@ public class CategoryService {
 	
 	//garante a integridade da transação do BD
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		//convertendo uma lista de Category para CategoryDTO
+		/**
+		 * fazendo uma implementação lâmbida(x->...)
+		 * .stream() - converte uma lista em funções de alta ordem
+		 * c/ expressões lâmbida(x->), função map() : transf cada elemento
+		 * original em outra coisa.
+		 * collect - transf uma stream() em uma lista
+ 		 */
 	}
 }
