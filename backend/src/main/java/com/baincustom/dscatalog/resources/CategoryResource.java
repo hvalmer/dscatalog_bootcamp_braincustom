abf9cf9dfc859a26a0890a7478ee61f7b45db013
hvalmer <hvalmer@gmail.com>
2021-06-23 23:28:27 -0300

    POST new Category

--- backend/src/main/java/com/baincustom/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/baincustom/dscatalog/resources/CategoryResource.java
@@ -1,13 +1,17 @@
 package com.baincustom.dscatalog.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.baincustom.dscatalog.dto.CategoryDTO;
 import com.baincustom.dscatalog.services.CategoryService;
@@ -37,5 +41,15 @@ public class CategoryResource {
 	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
 		CategoryDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	//inserindo novo objeto
+	@PostMapping //no Rest, para inserir um novo recurso usa-se o Post
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+		dto = service.insert(dto);
+		//inserindo no cabecalho da responsta no Postman o location do {id}
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				  .buildAndExpand(dto.getId()).toUri();	
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
